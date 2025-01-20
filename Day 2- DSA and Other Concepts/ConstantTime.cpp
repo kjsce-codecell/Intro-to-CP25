7737a63bccd7194ba10bf02b2a7cd7d76613a5ef
Purva <potepurva@gmail.com>
2025-01-20 15:33:37 +0530

    Added different files for examples

---
+++ Day 2- DSA and Other Concepts/ConstantTime.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[] = {10, 20, 30, 40, 50};
+
+    // Constant Time O(1)
+    cout << "First Element : " << arr[0] <<"\n";  
+    // Always takes the same time
+}
